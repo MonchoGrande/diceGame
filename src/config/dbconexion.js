12fd21a9d051d93f3e3527bf827f9e6c863d0ae9
Moncho <monchoforcout@gmail.com>
2021-10-30 18:48:39 +0200

    Create scripts dev - start

--- src/config/dbconexion.js
+++ src/config/dbconexion.js
@@ -1,11 +1,15 @@
 const mongoose = require('mongoose');
 
 mongoose
-  .connect('mongodb://localhost:27017/dados', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-  })
+  .connect(
+    `${process.env.PROTOCOL}://${process.env.URL}:${process.env.DB_PORT}/${process.env.DATABASE_NAME}`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    }
+  )
   .then(() => console.log('conexion establecida con MongoDB'))
   .catch((error) =>
     console.error('No se ha podido conectar con MongoDB:', error.message)
